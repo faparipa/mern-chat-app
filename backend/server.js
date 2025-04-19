63792ada22163687ffcaf702036c3acd6156d364
faparipa <farsang.gabor@gmail.com>
2025-04-19 17:25:03 +0200

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 
@@ -7,16 +8,14 @@ import userRoutes from './routes/user.routes.js';
 import connectToMongoDB from './db/connectTOMongoDB.js';
 import cookieParser from 'cookie-parser';
 import { app, server } from './socket/socket.js';
-//const app = express(); //áthelyezve a socket.js-be
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-// app.get('/', (req, res) => {
-//   //root rout http://localhost:5000/
-//   res.send('Server is ready!');
-// });
+//to deploy you need
+const __dirname = path.resolve();
+
 //midleware:
 app.use(express.json()); // to parse the incominng request with JSON payloads (from req.body)
 app.use(cookieParser());
@@ -24,6 +23,13 @@ app.use(cookieParser());
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/users', userRoutes);
+
+//to deploy
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+});
 
 server.listen(PORT, () => {
   connectToMongoDB();
